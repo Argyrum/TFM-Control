a8b7722902b02d0fdb51973a6d9db00f13d09820
Argyrum <Fact_core@outlook.com>
2025-04-25 01:17:54 +0200

    Optimized FindWnPrecise script

--- Matlab WS/Custom scripts/FindWnPrecise.m
+++ Matlab WS/Custom scripts/FindWnPrecise.m
@@ -1,4 +1,4 @@
-function [wEP, error, isStable, Kn] = FindWnPrecise(A, B, C, b, c, wini, precision)
+function [wEP, isStable, Kn] = FindWnPrecise(A, B, C, b, c, wini, precision)
 % Refines w at imag{J(w)}=-pi*b/(4*c) between wl(1) and wl(2) and computes
 % kn after checking stability
 
@@ -9,7 +9,10 @@ function [wEP, error, isStable, Kn] = FindWnPrecise(A, B, C, b, c, wini, precisi
 
     %options = optimset('Display', 'iter', 'TolX', precision);
     options = optimset('TolX', precision);
-    [wEP, error, exitflag] = fzero(@(w) imag(lprsmatr(A, B, C, w)) + pi*b/(4*c), wini, options);
+    obj_fun = @(w) abs(imag(lprsmatr(A, B, C, w)) + pi*b/(4*c));
+    
+	%[wEP, error, exitflag] = fzero(@(w) imag(lprsmatr(A, B, C, w)) + pi*b/(4*c), wini, options);
+    [wEP, ~, exitflag] = fminsearch(obj_fun, wini, options);
 
     if (exitflag ~= 1)
         fprintf('ERROR - Could not find solution\n');
@@ -20,7 +23,7 @@ function [wEP, error, isStable, Kn] = FindWnPrecise(A, B, C, b, c, wini, precisi
         fprintf('SUCCESS: wn = %f (Stable, kn = %f, f = %.2f Hz)\n', wEP, Kn, wEP/(2*pi))
 
     else
-        isStable = fail;
+        isStable = false;
         fprintf('FAIL: w = %f (Unstable, f = %.2f Hz)\n', wEP, wEP/(2*pi))
 
     end
