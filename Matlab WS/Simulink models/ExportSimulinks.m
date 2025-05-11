a89380daa550f37efd0205cc3497e4166010c9fb
Argyrum <Fact_core@outlook.com>
2025-05-11 22:00:25 +0200

    Add simulations and export mechanism

--- Matlab WS/Simulink models/ExportSimulinks.m
+++ Matlab WS/Simulink models/ExportSimulinks.m
@@ -1,4 +1,31 @@
-load_system("Oscilloscopi_offset.slx");
-Simulink.exportToVersion("Oscilloscopi_offset", "Oscilloscopi_offset2020a.slx", "R2020a");
-load_system("PlantaTFG.slx");
-Simulink.exportToVersion("PlantaTFG", "PlantaTFG2020a.slx", "R2020a");
+load_system("Simulink models\CLRFSG.slx");
+Simulink.exportToVersion("CLRFSG", "Simulink models\exported\CLRFSG_2022.slx", "R2022a");
+close_system
+
+load_system("Simulink models\CLRFSGa.slx");
+Simulink.exportToVersion("CLRFSGa", "Simulink models\exported\CLRFSGa_2022.slx", "R2022a");
+close_system
+
+load_system("Simulink models\CLRFSPFCRealized.slx");
+Simulink.exportToVersion("CLRFSPFCRealized", "Simulink models\exported\CLRFSPFCRealized_2022.slx", "R2022a");
+close_system
+
+load_system("Simulink models\CLRFSPFCRealizedSimscape.slx");
+Simulink.exportToVersion("CLRFSPFCRealizedSimscape", "Simulink models\exported\CLRFSPFCRealizedSimscape_2022.slx", "R2022a");
+close_system
+
+load_system("Simulink models\GcFilterTest.slx");
+Simulink.exportToVersion("GcFilterTest", "Simulink models\exported\GcFilterTest_2022.slx", "R2022a");
+close_system
+
+load_system("Simulink models\OLRippleTest.slx");
+Simulink.exportToVersion("OLRippleTest", "Simulink models\exported\OLRippleTest_2022.slx", "R2022a");
+close_system
+
+load_system("Simulink models\SSRTest.slx");
+Simulink.exportToVersion("SSRTest", "Simulink models\exported\SSRTest_2022.slx", "R2022a");
+close_system
+
+load_system("Simulink models\SumSimscapeTest.slx");
+Simulink.exportToVersion("SumSimscapeTest", "Simulink models\exported\SumSimscapeTest_2022.slx", "R2022a");
+close_system
